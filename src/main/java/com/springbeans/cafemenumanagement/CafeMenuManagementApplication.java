f370f6530ade9741dee8d6ade1959c7e6d514f1b
Ascody <actaney@gmail.com>
2026-07-18 11:04:05 +0900

    initial commit

---
+++ src/main/java/com/springbeans/cafemenumanagement/CafeMenuManagementApplication.java
@@ -0,0 +1,13 @@
+package com.springbeans.cafemenumanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeMenuManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeMenuManagementApplication.class, args);
+    }
+
+}
