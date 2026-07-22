98a3bd77687d691b6197b18a4f4681d4dd8c4bf0
kwonbeomjun <kjundwa@gmail.com>
2026-07-22 23:14:50 +0900

    feat: sales 통계 기능 구현

--- src/main/java/com/springbeans/cafemenumanagement/product/dto/ProductResponse.java
+++ src/main/java/com/springbeans/cafemenumanagement/product/dto/ProductResponse.java
@@ -2,6 +2,7 @@ package com.springbeans.cafemenumanagement.product.dto;
 
 import com.springbeans.cafemenumanagement.product.entity.Product;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
 
 public record ProductResponse(
 
